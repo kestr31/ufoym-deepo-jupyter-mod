439c16a51339d2d12de9265f5ee5dc1efe0183d2
kestr3l <samsani@naver.com>
2022-03-17 23:25:52 +0900

    New initialization script

---
+++ config/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Change Directory
+cd /root/.jupyter
+
+# Generate OpenSSL Certificate Valid for 730 Days
+openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
+-subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" \
+-keyout jupyter.key -out jupyter.cert 
