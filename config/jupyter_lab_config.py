d4486d72ac06d061f567ea44aacb4a4d74766a27
kestrel <samsani@naver.com>
2022-04-18 14:53:57 +0900

    Modified error in initial config file

--- config/jupyter_lab_config.py
+++ config/jupyter_lab_config.py
@@ -12,5 +12,5 @@ c.ServerApp.root_dir = '/root/.jupyter/lab/workspaces'
 c.ServerApp.password_required = False
 c.ServerApp.token = ''
 #c.ServerApp.password = ''
-c.ServerApp.certfile = u'/root/.jupyter/jupyter.pem'
+c.ServerApp.certfile = u'/root/.jupyter/jupyter.cert'
 c.ServerApp.keyfile = u'/root/.jupyter/jupyter.key'
