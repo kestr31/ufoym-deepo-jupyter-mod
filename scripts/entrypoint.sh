2bd275f568bcd6559fe2d89feaf951524c616d68
merlin <samsani@naver.com>
2022-07-05 20:56:30 +0900

    Auto-start SSH at startup

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,5 +8,8 @@ openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" \
 -keyout jupyter.key -out jupyter.cert 
 
+# RUN  SSH
+service ssh restart
+
 # Run Jupyter Lab
 jupyter lab --config=/root/.jupyter/jupyter_lab_config.py
